781570749a796df6dfe46765f5ffa398cdbd1e92
Viraj <virajmalaka94@gmail.com>
2023-05-02 17:19:47 +0530

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var charactor = "viraj";
+console.log(charactor);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+const charactor = "viraj"
+
+console.log(charactor);
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input=>{
+    console.log(input);
+    
+})
